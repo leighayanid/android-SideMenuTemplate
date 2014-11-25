ff6e1edcc7ad39e5563a8576907fbabf9cf1472e
leighayanid <mr.khizzer04@gmail.com>
2014-11-25 09:56:40 -0800

    added custom row for side menu

--- gen/com/example/navigation/R.java
+++ gen/com/example/navigation/R.java
@@ -1083,8 +1083,17 @@ containing a value of this type.
         public static final int b1=0x7f020057;
         public static final int button_trans=0x7f020058;
         public static final int ic_drawer=0x7f020059;
-        public static final int ic_launcher=0x7f02005a;
-        public static final int logo=0x7f02005b;
+        public static final int ic_events=0x7f02005a;
+        public static final int ic_festival=0x7f02005b;
+        public static final int ic_home=0x7f02005c;
+        public static final int ic_hotel=0x7f02005d;
+        public static final int ic_landmark=0x7f02005e;
+        public static final int ic_launcher=0x7f02005f;
+        public static final int ic_naturalwonders=0x7f020060;
+        public static final int ic_products=0x7f020061;
+        public static final int ic_resortandbeaches=0x7f020062;
+        public static final int ic_resto=0x7f020063;
+        public static final int logo=0x7f020064;
     }
     public static final class id {
         public static final int action_bar=0x7f09001c;
